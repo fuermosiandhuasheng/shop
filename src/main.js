6154c7f2db3e61b3da288ef5d13be1d111ce0d1f
shixian <shixian@qq.com>
2019-11-03 15:59:07 +0800

    登陆页面完成

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+// 字体图标
+import './assets/fonts/iconfont.css'
+// 全局css
+import './assets/css/global.css'
+// 全局导入axios
+import axios from 'axios'
+// 配置跟路径
+axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
