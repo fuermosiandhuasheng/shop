90d79c96de99ba1f2d692f721c4ee66ee2467758
shixian <shixian@qq.com>
2019-11-06 09:23:11 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -10,8 +10,11 @@ import './assets/css/global.css'
 import axios from 'axios'
 // 配置跟路径
 axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
-
 Vue.config.productionTip = false
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,20 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 Vue.use(VueRouter)
-
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 const routes = [
   { path: '/', redirect: '/Login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home }
+  { path: '/home', component: Home, redirect:'/welcome', children:[
+    {path:'/welcome', component: Welcome},
+    {path:'/users', component: Users}
+  ]}
 ]
 
 const router = new VueRouter({
